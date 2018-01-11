3bdb2aae7b2eba81996cd047f888b86de6ffbfd8
shuymn <mail@shuymn.me>
2018-01-12 05:13:06 +0900

    Add translation-button-hider.js

---
+++ src/js/module/translation-button-hider.js
@@ -0,0 +1,30 @@
+export default class TranslationButtonHider {
+  /**
+   * Inject a css.
+   * @returns {Promise}
+   */
+  static run() {
+    const link = document.createElement('link');
+    link.href = chrome.extension.getURL('css/content.css');
+    link.type = 'text/css';
+    link.rel = 'stylesheet';
+
+    const target = document.querySelector('head');
+
+    if (target === null) {
+      return Promise.reject();
+    }
+
+    target.appendChild(link);
+
+    return Promise.resolve();
+  }
+
+  /**
+   * Do nothing.
+   * @returns {Promise}
+   */
+  static stop() {
+    return Promise.resolve();
+  }
+}
