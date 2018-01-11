f454bc8c75a4749ff6057d191461e01ad2abebd4
shuymn <mail@shuymn.me>
2018-01-12 05:13:32 +0900

    Add content script

---
+++ src/js/content.js
@@ -0,0 +1,58 @@
+import NewTweetLoader from './module/new-tweet-loader';
+import TranslationButtonHider from './module/translation-button-hider';
+
+/**
+ * Stop all features.
+ * @param {Function[]} features
+ * @return {Promise}
+ */
+function stop(features) {
+  return Promise.all(features.map((feature) => {
+    return feature.stop();
+  }));
+}
+
+/**
+ * Execute features which are executed only once.
+ * @param {Function[]} features
+ */
+function once(features) {
+  features.forEach((feature) => {
+    feature.run();
+  });
+}
+
+/**
+ * Execute features which reside.
+ * @param {Function[]} features
+ */
+function resident(features) {
+  chrome.runtime.onMessage.addListener(async (request) => {
+    /**
+     * @param {RegExp} pattern
+     */
+    function check(pattern) {
+      return pattern.test(request.url);
+    }
+
+    await stop(features);
+
+    features.forEach((feature) => {
+      const patterns = feature.getPatterns();
+
+      if (patterns.some(check)) {
+        feature.run();
+      }
+    });
+  });
+}
+
+// fire
+
+once([
+  TranslationButtonHider,
+]);
+
+resident([
+  NewTweetLoader,
+]);
