d8295d57e45dc9623b6311389b282df553600b44
shuymn <mail@shuymn.me>
2018-01-14 00:20:53 +0900

    Split code around execution

--- src/js/content.js
+++ src/js/content.js
@@ -1,48 +1,9 @@
 import NewTweetLoader from './module/new-tweet-loader';
 import TranslationButtonHider from './module/translation-button-hider';
-
-/**
- * Stop all features.
- * @param {Function[]} features
- * @return {Promise}
- */
-function stop(features) {
-  return Promise.all(features.map(feature => feature.stop()));
-}
-
-/**
- * Execute features which are executed only once.
- * @param {Function[]} features
- * @return {Promise}
- */
-function once(features) {
-  return Promise.all(features.map(feature => feature.run()));
-}
-
-/**
- * Execute features which reside.
- * @param {Function[]} features
- */
-function resident(features) {
-  chrome.runtime.onMessage.addListener(async (request) => {
-    /**
-     * @param {RegExp} pattern
-     */
-    function check(pattern) {
-      return pattern.test(request.url);
-    }
-
-    await stop(features);
-
-    features.forEach((feature) => {
-      const patterns = feature.getPatterns();
-
-      if (patterns.some(check)) {
-        feature.run();
-      }
-    });
-  });
-}
+import {
+  once,
+  resident,
+} from './module/content-helper';
 
 // fire
 
---
+++ src/js/module/content-helper.js
@@ -0,0 +1,42 @@
+/**
+ * Stop all features.
+ * @param {Array<Function>} features
+ * @return {Promise<any[]>}
+ */
+function stop(features) {
+  return Promise.all(features.map(feature => feature.stop()));
+}
+
+/**
+ * Execute features which are executed only once.
+ * @param {Array<Function>} features
+ * @return {Promise<Array>}
+ */
+function once(features) {
+  return Promise.all(features.map(feature => feature.run()));
+}
+
+/**
+ * Execute features which reside.
+ * @param {(Array<Function>|Array<Array>)} _features
+ */
+function resident(features) {
+  chrome.runtime.onMessage.addListener(async (request) => {
+    /**
+     * @param {RegExp} pattern
+     */
+    function check(pattern) {
+      return pattern.test(request.url);
+    }
+
+    await stop(features);
+
+    features.forEach((feature) => {
+      const patterns = feature.getPatterns();
+
+      if (patterns.some(check)) {
+        feature.run();
+      }
+    });
+  });
+}
