7532de97624813213052eaa40a15992c5c67fc5f
shuymn <mail@shuymn.me>
2018-01-23 00:46:47 +0900

    Update webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,14 +1,18 @@
 const mix = require('laravel-mix'); // eslint-disable-line import/no-extraneous-dependencies
 
-mix.js('./src/js/content.js', './dist/js')
-  .js('./src/js/background.js', './dist/js')
-  .js('./src/js/options.js', './dist/js')
-  .standaloneSass('./src/css/options.scss', './dist/css')
-  .standaloneSass('./src/css/module/translation-button-hider.scss', './dist/css')
-  .copy('./src/static/**/*', './dist')
-  .copy('./src/js/analytics.js', './dist/js')
-  .copyDirectory('./src/locale/en', './dist/_locales/en')
-  .copyDirectory('./src/locale/ja', './dist/_locales/ja')
+mix.js('./src/js/content.js', './build/js')
+  .js('./src/js/background.js', './build/js')
+  .js('./src/js/options.js', './build/js')
+  .standaloneSass('./src/css/options.scss', './build/css')
+  .standaloneSass('./src/css/module/translation-button-hider.scss', './build/css')
+  .copyDirectory('./src/locale/en', './build/_locales/en')
+  .copyDirectory('./src/locale/ja', './build/_locales/ja')
+  .copy('./src/js/analytics.js', './build/js')
+  .copy('./src/static/*.html', './build')
+  .copyDirectory('./build', './dist/chrome')
+  .copyDirectory('./build', './dist/firefox')
+  .copy('./src/static/manifest.chrome.json', './dist/chrome/manifest.json')
+  .copy('./src/static/manifest.firefox.json', './dist/firefox/manifest.json')
   .disableNotifications();
 
 if (!mix.inProduction()) {
